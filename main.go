d28128f38b9e95a0a36cdcf49ef0a910f70b334f
aulia.sutedjo <aulianurhadisutedjo@bluebirdgroup.com>
2020-08-25 09:09:15 +0700

    implement firebase notification

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Request struct {
+	Notification PayloadFCM `json:"notification"`
+	To           string     `json:"to"`
+}
+
+type PayloadFCM struct {
+	Title       string `json:"title"`
+	Body        string `json:"body"`
+	Icon        string `json:"icon,omitempty"`
+	ClickAction string `json:"click_action,omitempty"`
+}
+
+type Response struct {
+	MulticastID  int64           `json:"multicast_id"`
+	Success      int             `json:"success"`
+	Failure      int             `json:"failure"`
+	CanonicalIds int             `json:"canonical_ids"`
+	Results      []PayloadResult `json:"results"`
+}
+
+type PayloadResult struct {
+	MessageID string `json:"message_id"`
+}
+
+func main() {
+	reqStruct := &Request{
+		Notification: PayloadFCM{
+			Title: "Reminder Meeting",
+			Body:  "Sebentar lagi meeting, brow!",
+		},
+		To: "",
+	}
+	requestBody, err := json.Marshal(reqStruct)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	httpRequest, err := http.NewRequest("POST", "https://fcm.googleapis.com/fcm/send", bytes.NewBuffer(requestBody))
+	httpRequest.Header.Set("Content-Type", "application/json")
+	httpRequest.Header.Set("Authorization", "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(httpRequest)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var respBody Response
+	err = json.Unmarshal(body, &respBody)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Body: ", respBody)
+}
